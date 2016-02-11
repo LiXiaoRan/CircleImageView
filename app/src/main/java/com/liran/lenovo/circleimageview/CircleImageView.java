535d4e5576be69abf1123b2d8f50ddd672b1f861
liran <qq997843911@gmail.com>
2016-02-11 15:20:33 +0800

    添加部分逻辑

--- app/src/main/java/com/liran/lenovo/circleimageview/CircleImageView.java
+++ app/src/main/java/com/liran/lenovo/circleimageview/CircleImageView.java
@@ -2,10 +2,15 @@ package com.liran.lenovo.circleimageview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.widget.ImageView;
@@ -57,6 +62,9 @@ public class CircleImageView extends ImageView {
     private int mWidth;
 
     private RectF mRoundRect;
+    private Canvas Bitmapcanvas;
+    private Bitmap creatBitmap;
+    private float scale;
 
     public CircleImageView(Context context) {
         super(context);
@@ -77,6 +85,7 @@ public class CircleImageView extends ImageView {
 
         mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mMatrix = new Matrix();
+        scale = 0;
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView);
 
@@ -100,4 +109,55 @@ public class CircleImageView extends ImageView {
         }
 
     }
+
+    /**
+     * 初始化bitmapshader
+     */
+    private void setUpShader() {
+
+        Drawable drawable = getDrawable();
+        if (drawable == null) {
+            return;
+        }
+
+        Bitmap bmp = drawableToBitamp(drawable);
+        mBitmapShader = new BitmapShader(bmp, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        if (type == TYPE_CIRCLE) {
+            scale = mWidth * 1.0f / Math.min(bmp.getWidth(), bmp.getHeight());
+        } else if (type == TYPE_ROUND) {
+            // 如果图片的宽或者高与view的宽高不匹配，计算出需要缩放的比例；缩放后的图片的宽高，一定要大于我们view的宽高；所以我们这里取大值；
+            scale = Math.max(getWidth() * 1.0f / bmp.getWidth(), getHeight()
+                    * 1.0f / bmp.getHeight());
+        }
+        // shader的变换矩阵，我们这里主要用于放大或者缩小
+        mMatrix.setScale(scale, scale);
+        //设置变换矩阵
+        mBitmapShader.setLocalMatrix(mMatrix);
+
+        // 设置shader
+        mBitmapPaint.setShader(mBitmapShader);
+    }
+
+    /**
+     * drawable幢bitmap
+     *
+     * @param drawable
+     * @return
+     */
+    private Bitmap drawableToBitamp(Drawable drawable) {
+
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int w = drawable.getIntrinsicWidth();
+        int h = drawable.getIntrinsicHeight();
+        creatBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Bitmapcanvas = new Canvas(creatBitmap);
+        drawable.setBounds(0, 0, w, h);
+        drawable.draw(Bitmapcanvas);
+        
+        return creatBitmap;
+    }
+
 }
